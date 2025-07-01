6ea977df54e79a27b3c38b8a1a84092b72f40990
Puneetha S <119097022+puneethaQ12@users.noreply.github.com>
2025-07-01 16:57:54 +0530

    Add files via upload

---
+++ _Basic Transformation - I.py
@@ -0,0 +1,725 @@
+# Databricks notebook source
+# Spark Session
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession
+    .builder
+    .appName("Basic Transformation - I")
+    .master("local[*]")
+    .getOrCreate()
+)
+
+spark
+
+
+# COMMAND ----------
+
+#Spark session 
+from pyspark.sql import SparkSession
+spark=(
+    SparkSession
+    .builder
+    .appName("Spark_Intro")
+    .master("local[*]")
+    .getOrCreate()
+)
+
+# COMMAND ----------
+
+spark
+
+# COMMAND ----------
+
+# Emp Data & Schema
+
+emp_data = [
+    ["001","101","John Doe","30","Male","50000","2015-01-01"],
+    ["002","101","Jane Smith","25","Female","45000","2016-02-15"],
+    ["003","102","Bob Brown","35","Male","55000","2014-05-01"],
+    ["004","102","Alice Lee","28","Female","48000","2017-09-30"],
+    ["005","103","Jack Chan","40","Male","60000","2013-04-01"],
+    ["006","103","Jill Wong","32","Female","52000","2018-07-01"],
+    ["007","101","James Johnson","42","Male","70000","2012-03-15"],
+    ["008","102","Kate Kim","29","Female","51000","2019-10-01"],
+    ["009","103","Tom Tan","33","Male","58000","2016-06-01"],
+    ["010","104","Lisa Lee","27","Female","47000","2018-08-01"],
+    ["011","104","David Park","38","Male","65000","2015-11-01"],
+    ["012","105","Susan Chen","31","Female","54000","2017-02-15"],
+    ["013","106","Brian Kim","45","Male","75000","2011-07-01"],
+    ["014","107","Emily Lee","26","Female","46000","2019-01-01"],
+    ["015","106","Michael Lee","37","Male","63000","2014-09-30"],
+    ["016","107","Kelly Zhang","30","Female","49000","2018-04-01"],
+    ["017","105","George Wang","34","Male","57000","2016-03-15"],
+    ["018","104","Nancy Liu","29","Female","50000","2017-06-01"],
+    ["019","103","Steven Chen","36","Male","62000","2015-08-01"],
+    ["020","102","Grace Kim","32","Female","53000","2018-11-01"]
+]
+
+
+emp_schema = "employee_id string, department_id string, name string, age string, gender string, salary string, hire_date string"
+
+
+# COMMAND ----------
+
+# Create emp DataFrame
+
+emp = spark.createDataFrame(data=emp_data, schema=emp_schema)
+
+# COMMAND ----------
+
+
+# Check number of partitions
+emp.rdd.getNumPartitions()
+
+
+# COMMAND ----------
+
+#show data
+emp.show()
+
+# COMMAND ----------
+
+# Write our first Transformation (EMP salary > 50000)
+emp_first=emp.where("salary>50000")
+emp_first.show()
+
+# COMMAND ----------
+
+# Validate number of Partitions
+
+emp_first.rdd.getNumPartitions()
+
+# COMMAND ----------
+
+emp_first.write.format("csv").save("data/output/1/emp.csv")
+
+
+# COMMAND ----------
+
+#Schema for Emp
+emp.schema
+
+# COMMAND ----------
+
+# example for Schema
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+schema_string = "name string, age int"
+
+schema_spark = StructType([
+    StructField("name", StringType(), True),
+    StructField("age", IntegerType(), True)
+])
+
+
+# COMMAND ----------
+
+#Columns and expression
+from pyspark.sql.functions import col,expr
+emp.salary
+#emp['salary']  //one more way to call column
+
+# COMMAND ----------
+
+#select columns
+#select employee_id,name,age,salary
+
+
+emp_filtered=emp.select(col("employee_id"),expr("name"),emp.age,emp.salary)
+
+# COMMAND ----------
+
+
+# SHOW Dataframe (ACTION)
+
+emp_filtered.show()
+
+# COMMAND ----------
+
+# Using expr for select
+# select employee_id as emp_id, name, cast(age as int) as age, salary from emp_filtered
+
+emp_casted = emp_filtered.select(expr("employee_id as emp_id"), emp.name, expr("cast(age as int) as age"), emp.salary)
+
+
+# COMMAND ----------
+
+emp_casted.show()
+
+
+# COMMAND ----------
+
+emp_casted_1 = emp_filtered.selectExpr("employee_id as emp_id", "name", "cast(age as int) as age", "salary")
+emp_casted_1.show()
+
+# COMMAND ----------
+
+emp_casted.printSchema()
+
+
+# COMMAND ----------
+
+# Filter emp based on Age > 30
+# select emp_id, name, age, salary from emp_casted where age > 30
+
+emp_fil=emp_casted.select("emp_id","name", "age", "salary").where("age>30")
+
+emp_fil.show()
+
+# COMMAND ----------
+
+# Write the data back as CSV (ACTION)
+
+emp_fil.write.format("csv").csv("data/output/2/emp.csv")
+
+# COMMAND ----------
+
+# Bonus TIP
+
+schema_str = "name string, age int"
+
+from pyspark.sql.types import _parse_datatype_string
+
+schema_spark = _parse_datatype_string(schema_str)
+
+schema_spark
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+#Spark session 
+from pyspark.sql import SparkSession
+spark=(
+    SparkSession
+    .builder
+    .appName("Spark_Intro")
+    .master("local[*]")
+    .getOrCreate()
+)
+
+# COMMAND ----------
+
+spark
+
+# COMMAND ----------
+
+# Emp Data & Schema
+
+emp_data = [
+    ["001","101","John Doe","30","Male","50000","2015-01-01"],
+    ["002","101","Jane Smith","25","Female","45000","2016-02-15"],
+    ["003","102","Bob Brown","35","Male","55000","2014-05-01"],
+    ["004","102","Alice Lee","28","Female","48000","2017-09-30"],
+    ["005","103","Jack Chan","40","Male","60000","2013-04-01"],
+    ["006","103","Jill Wong","32","Female","52000","2018-07-01"],
+    ["007","101","James Johnson","42","Male","70000","2012-03-15"],
+    ["008","102","Kate Kim","29","Female","51000","2019-10-01"],
+    ["009","103","Tom Tan","33","Male","58000","2016-06-01"],
+    ["010","104","Lisa Lee","27","Female","47000","2018-08-01"],
+    ["011","104","David Park","38","Male","65000","2015-11-01"],
+    ["012","105","Susan Chen","31","Female","54000","2017-02-15"],
+    ["013","106","Brian Kim","45","Male","75000","2011-07-01"],
+    ["014","107","Emily Lee","26","Female","46000","2019-01-01"],
+    ["015","106","Michael Lee","37","Male","63000","2014-09-30"],
+    ["016","107","Kelly Zhang","30","Female","49000","2018-04-01"],
+    ["017","105","George Wang","34","Male","57000","2016-03-15"],
+    ["018","104","Nancy Liu","29","Female","50000","2017-06-01"],
+    ["019","103","Steven Chen","36","Male","62000","2015-08-01"],
+    ["020","102","Grace Kim","32","Female","53000","2018-11-01"]
+]
+
+
+emp_schema = "employee_id string, department_id string, name string, age string, gender string, salary string, hire_date string"
+
+
+# COMMAND ----------
+
+# Create emp DataFrame
+
+emp = spark.createDataFrame(data=emp_data, schema=emp_schema)
+
+# COMMAND ----------
+
+
+# Check number of partitions
+emp.rdd.getNumPartitions()
+
+
+# COMMAND ----------
+
+#show data
+emp.show()
+
+# COMMAND ----------
+
+# Write our first Transformation (EMP salary > 50000)
+emp_first=emp.where("salary>50000")
+emp_first.show()
+
+# COMMAND ----------
+
+# Validate number of Partitions
+
+emp_first.rdd.getNumPartitions()
+
+# COMMAND ----------
+
+emp_first.write.format("csv").save("data/output/1/emp.csv")
+
+
+# COMMAND ----------
+
+#Schema for Emp
+emp.schema
+
+# COMMAND ----------
+
+# example for Schema
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+schema_string = "name string, age int"
+
+schema_spark = StructType([
+    StructField("name", StringType(), True),
+    StructField("age", IntegerType(), True)
+])
+
+
+# COMMAND ----------
+
+#Columns and expression
+from pyspark.sql.functions import col,expr
+emp.salary
+#emp['salary']  //one more way to call column
+
+# COMMAND ----------
+
+#select columns
+#select employee_id,name,age,salary
+
+
+emp_filtered=emp.select(col("employee_id"),expr("name"),emp.age,emp.salary)
+
+# COMMAND ----------
+
+
+# SHOW Dataframe (ACTION)
+
+emp_filtered.show()
+
+# COMMAND ----------
+
+# Using expr for select
+# select employee_id as emp_id, name, cast(age as int) as age, salary from emp_filtered
+
+emp_casted = emp_filtered.select(expr("employee_id as emp_id"), emp.name, expr("cast(age as int) as age"), emp.salary)
+
+
+# COMMAND ----------
+
+emp_casted.show()
+
+
+# COMMAND ----------
+
+emp_casted_1 = emp_filtered.selectExpr("employee_id as emp_id", "name", "cast(age as int) as age", "salary")
+emp_casted_1.show()
+
+# COMMAND ----------
+
+emp_casted.printSchema()
+
+
+# COMMAND ----------
+
+# Filter emp based on Age > 30
+# select emp_id, name, age, salary from emp_casted where age > 30
+
+emp_fil=emp_casted.select("emp_id","name", "age", "salary").where("age>30")
+
+emp_fil.show()
+
+# COMMAND ----------
+
+# Write the data back as CSV (ACTION)
+
+emp_fil.write.format("csv").csv("data/output/2/emp.csv")
+
+# COMMAND ----------
+
+# Bonus TIP
+
+schema_str = "name string, age int"
+
+from pyspark.sql.types import _parse_datatype_string
+
+schema_spark = _parse_datatype_string(schema_str)
+
+schema_spark
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+#Spark session 
+from pyspark.sql import SparkSession
+spark=(
+    SparkSession
+    .builder
+    .appName("Spark_Intro")
+    .master("local[*]")
+    .getOrCreate()
+)
+
+# COMMAND ----------
+
+spark
+
+# COMMAND ----------
+
+# Emp Data & Schema
+
+emp_data = [
+    ["001","101","John Doe","30","Male","50000","2015-01-01"],
+    ["002","101","Jane Smith","25","Female","45000","2016-02-15"],
+    ["003","102","Bob Brown","35","Male","55000","2014-05-01"],
+    ["004","102","Alice Lee","28","Female","48000","2017-09-30"],
+    ["005","103","Jack Chan","40","Male","60000","2013-04-01"],
+    ["006","103","Jill Wong","32","Female","52000","2018-07-01"],
+    ["007","101","James Johnson","42","Male","70000","2012-03-15"],
+    ["008","102","Kate Kim","29","Female","51000","2019-10-01"],
+    ["009","103","Tom Tan","33","Male","58000","2016-06-01"],
+    ["010","104","Lisa Lee","27","Female","47000","2018-08-01"],
+    ["011","104","David Park","38","Male","65000","2015-11-01"],
+    ["012","105","Susan Chen","31","Female","54000","2017-02-15"],
+    ["013","106","Brian Kim","45","Male","75000","2011-07-01"],
+    ["014","107","Emily Lee","26","Female","46000","2019-01-01"],
+    ["015","106","Michael Lee","37","Male","63000","2014-09-30"],
+    ["016","107","Kelly Zhang","30","Female","49000","2018-04-01"],
+    ["017","105","George Wang","34","Male","57000","2016-03-15"],
+    ["018","104","Nancy Liu","29","Female","50000","2017-06-01"],
+    ["019","103","Steven Chen","36","Male","62000","2015-08-01"],
+    ["020","102","Grace Kim","32","Female","53000","2018-11-01"]
+]
+
+
+emp_schema = "employee_id string, department_id string, name string, age string, gender string, salary string, hire_date string"
+
+
+# COMMAND ----------
+
+# Create emp DataFrame
+
+emp = spark.createDataFrame(data=emp_data, schema=emp_schema)
+
+# COMMAND ----------
+
+
+# Check number of partitions
+emp.rdd.getNumPartitions()
+
+
+# COMMAND ----------
+
+#show data
+emp.show()
+
+# COMMAND ----------
+
+# Write our first Transformation (EMP salary > 50000)
+emp_first=emp.where("salary>50000")
+emp_first.show()
+
+# COMMAND ----------
+
+# Validate number of Partitions
+
+emp_first.rdd.getNumPartitions()
+
+# COMMAND ----------
+
+emp_first.write.format("csv").save("data/output/1/emp.csv")
+
+
+# COMMAND ----------
+
+#Schema for Emp
+emp.schema
+
+# COMMAND ----------
+
+# example for Schema
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+schema_string = "name string, age int"
+
+schema_spark = StructType([
+    StructField("name", StringType(), True),
+    StructField("age", IntegerType(), True)
+])
+
+
+# COMMAND ----------
+
+#Columns and expression
+from pyspark.sql.functions import col,expr
+emp.salary
+#emp['salary']  //one more way to call column
+
+# COMMAND ----------
+
+#select columns
+#select employee_id,name,age,salary
+
+
+emp_filtered=emp.select(col("employee_id"),expr("name"),emp.age,emp.salary)
+
+# COMMAND ----------
+
+
+# SHOW Dataframe (ACTION)
+
+emp_filtered.show()
+
+# COMMAND ----------
+
+# Using expr for select
+# select employee_id as emp_id, name, cast(age as int) as age, salary from emp_filtered
+
+emp_casted = emp_filtered.select(expr("employee_id as emp_id"), emp.name, expr("cast(age as int) as age"), emp.salary)
+
+
+# COMMAND ----------
+
+emp_casted.show()
+
+
+# COMMAND ----------
+
+emp_casted_1 = emp_filtered.selectExpr("employee_id as emp_id", "name", "cast(age as int) as age", "salary")
+emp_casted_1.show()
+
+# COMMAND ----------
+
+emp_casted.printSchema()
+
+
+# COMMAND ----------
+
+# Filter emp based on Age > 30
+# select emp_id, name, age, salary from emp_casted where age > 30
+
+emp_fil=emp_casted.select("emp_id","name", "age", "salary").where("age>30")
+
+emp_fil.show()
+
+# COMMAND ----------
+
+# Write the data back as CSV (ACTION)
+
+emp_fil.write.format("csv").csv("data/output/2/emp.csv")
+
+# COMMAND ----------
+
+# Bonus TIP
+
+schema_str = "name string, age int"
+
+from pyspark.sql.types import _parse_datatype_string
+
+schema_spark = _parse_datatype_string(schema_str)
+
+schema_spark
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+#Spark session 
+from pyspark.sql import SparkSession
+spark=(
+    SparkSession
+    .builder
+    .appName("Spark_Intro")
+    .master("local[*]")
+    .getOrCreate()
+)
+
+# COMMAND ----------
+
+spark
+
+# COMMAND ----------
+
+# Emp Data & Schema
+
+emp_data = [
+    ["001","101","John Doe","30","Male","50000","2015-01-01"],
+    ["002","101","Jane Smith","25","Female","45000","2016-02-15"],
+    ["003","102","Bob Brown","35","Male","55000","2014-05-01"],
+    ["004","102","Alice Lee","28","Female","48000","2017-09-30"],
+    ["005","103","Jack Chan","40","Male","60000","2013-04-01"],
+    ["006","103","Jill Wong","32","Female","52000","2018-07-01"],
+    ["007","101","James Johnson","42","Male","70000","2012-03-15"],
+    ["008","102","Kate Kim","29","Female","51000","2019-10-01"],
+    ["009","103","Tom Tan","33","Male","58000","2016-06-01"],
+    ["010","104","Lisa Lee","27","Female","47000","2018-08-01"],
+    ["011","104","David Park","38","Male","65000","2015-11-01"],
+    ["012","105","Susan Chen","31","Female","54000","2017-02-15"],
+    ["013","106","Brian Kim","45","Male","75000","2011-07-01"],
+    ["014","107","Emily Lee","26","Female","46000","2019-01-01"],
+    ["015","106","Michael Lee","37","Male","63000","2014-09-30"],
+    ["016","107","Kelly Zhang","30","Female","49000","2018-04-01"],
+    ["017","105","George Wang","34","Male","57000","2016-03-15"],
+    ["018","104","Nancy Liu","29","Female","50000","2017-06-01"],
+    ["019","103","Steven Chen","36","Male","62000","2015-08-01"],
+    ["020","102","Grace Kim","32","Female","53000","2018-11-01"]
+]
+
+
+emp_schema = "employee_id string, department_id string, name string, age string, gender string, salary string, hire_date string"
+
+
+# COMMAND ----------
+
+# Create emp DataFrame
+
+emp = spark.createDataFrame(data=emp_data, schema=emp_schema)
+
+# COMMAND ----------
+
+
+# Check number of partitions
+emp.rdd.getNumPartitions()
+
+
+# COMMAND ----------
+
+#show data
+emp.show()
+
+# COMMAND ----------
+
+# Write our first Transformation (EMP salary > 50000)
+emp_first=emp.where("salary>50000")
+emp_first.show()
+
+# COMMAND ----------
+
+# Validate number of Partitions
+
+emp_first.rdd.getNumPartitions()
+
+# COMMAND ----------
+
+emp_first.write.format("csv").save("data/output/1/emp.csv")
+
+
+# COMMAND ----------
+
+#Schema for Emp
+emp.schema
+
+# COMMAND ----------
+
+# example for Schema
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+schema_string = "name string, age int"
+
+schema_spark = StructType([
+    StructField("name", StringType(), True),
+    StructField("age", IntegerType(), True)
+])
+
+
+# COMMAND ----------
+
+#Columns and expression
+from pyspark.sql.functions import col,expr
+emp.salary
+#emp['salary']  //one more way to call column
+
+# COMMAND ----------
+
+#select columns
+#select employee_id,name,age,salary
+
+
+emp_filtered=emp.select(col("employee_id"),expr("name"),emp.age,emp.salary)
+
+# COMMAND ----------
+
+
+# SHOW Dataframe (ACTION)
+
+emp_filtered.show()
+
+# COMMAND ----------
+
+# Using expr for select
+# select employee_id as emp_id, name, cast(age as int) as age, salary from emp_filtered
+
+emp_casted = emp_filtered.select(expr("employee_id as emp_id"), emp.name, expr("cast(age as int) as age"), emp.salary)
+
+
+# COMMAND ----------
+
+emp_casted.show()
+
+
+# COMMAND ----------
+
+emp_casted_1 = emp_filtered.selectExpr("employee_id as emp_id", "name", "cast(age as int) as age", "salary")
+emp_casted_1.show()
+
+# COMMAND ----------
+
+emp_casted.printSchema()
+
+
+# COMMAND ----------
+
+# Filter emp based on Age > 30
+# select emp_id, name, age, salary from emp_casted where age > 30
+
+emp_fil=emp_casted.select("emp_id","name", "age", "salary").where("age>30")
+
+emp_fil.show()
+
+# COMMAND ----------
+
+# Write the data back as CSV (ACTION)
+
+emp_fil.write.format("csv").csv("data/output/2/emp.csv")
+
+# COMMAND ----------
+
+# Bonus TIP
+
+schema_str = "name string, age int"
+
+from pyspark.sql.types import _parse_datatype_string
+
+schema_spark = _parse_datatype_string(schema_str)
+
+schema_spark
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
